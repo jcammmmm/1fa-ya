815ac9dbcec509963cc315f8336df980b0c469e6
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-06-03 12:02:54 -0500

    redux store added

--- utils/commands.cmd
+++ utils/commands.cmd
@@ -5,3 +5,5 @@ django-admin startproject leadmanager
 python.exe .\manage.py startapp leads
 python .\manage.py makemigrations leads
 python .\manage.py migrate
+
+npm i redux react-redux redux-thunk redux-devtools-extension
