6ca83e3d1016cbb4dad51325302b5be0beb26eed
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-06-02 22:31:33 -0500

    react component placeholders

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+  render() {
+    return (
+      <div>
+        <pre>Crear Formulario aquí.</pre>
+      </div>
+    )
+  }
+}
+
+export default Form
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <pre>Lista aquí.</pre>
+      </div>
+    )
+  }
+}
+
+export default Leads
