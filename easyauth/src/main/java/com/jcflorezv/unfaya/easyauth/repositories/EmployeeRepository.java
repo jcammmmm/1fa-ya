a70319def768fe2ec0bfd5330e4f028f60bdf155
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-06-23 13:55:07 -0500

    Navigating by Relationship

--- easyauth/src/main/java/com/jcflorezv/unfaya/easyauth/repositories/EmployeeRepository.java
+++ easyauth/src/main/java/com/jcflorezv/unfaya/easyauth/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.jcflorezv.unfaya.easyauth.repositories;
 
 import com.jcflorezv.unfaya.easyauth.entities.Employee;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {}
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+
+}
--- easyauth/src/main/js/app.js
+++ easyauth/src/main/js/app.js
@@ -2,6 +2,9 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client')
 
+const root = '/api';
+const follow = require('./follow'); // function to hop multiple links by "rel"
+
 class App extends React.Component {
 
 	constructor(props) {
@@ -10,10 +13,29 @@ class App extends React.Component {
 	}
 
 	componentDidMount() { 
-    client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-    });
+    this.loadFromServer(this.state.pageSize);
   }
+  
+  loadFromServer(pageSize) {
+		follow(client, root, [
+			{rel: 'employees', params: {size: pageSize}}]
+		).then(employeeCollection => {
+			return client({
+				method: 'GET',
+				path: employeeCollection.entity._links.profile.href,
+				headers: {'Accept': 'application/schema+json'}
+			}).then(schema => {
+				this.schema = schema.entity;
+				return employeeCollection;
+			});
+		}).done(employeeCollection => {
+			this.setState({
+				employees: employeeCollection.entity._embedded.employees,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: pageSize,
+				links: employeeCollection.entity._links});
+		});
+	}
 
 	render() {
 		return (
