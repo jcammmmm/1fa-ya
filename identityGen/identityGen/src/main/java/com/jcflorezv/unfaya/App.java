b5649f06167c0b4938728915c83849e84ca8d233
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-07-06 19:04:20 -0500

    indentity generator maven base files 2

---
+++ identityGen/identityGen/src/main/java/com/jcflorezv/unfaya/App.java
@@ -0,0 +1,13 @@
+package com.jcflorezv.unfaya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
