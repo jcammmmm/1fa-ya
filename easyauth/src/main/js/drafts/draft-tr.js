a10003a4e00b5ca744e7a055627afaaf34af3462
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-07-11 10:48:40 -0500

    added babel cli and plugin

---
+++ easyauth/src/main/js/drafts/draft-tr.js
@@ -0,0 +1,25 @@
+var a = function () {};
+
+var a = function (b) {
+  return b;
+};
+
+const double = [1, 2, 3].map(function (num) {
+  return num * 2;
+});
+console.log(double); // [2,4,6]
+
+var bob = {
+  _name: "Bob",
+  _friends: ["Sally", "Tom"],
+
+  printFriends() {
+    var _this = this;
+
+    this._friends.forEach(function (f) {
+      return console.log(_this._name + " knows " + f);
+    });
+  }
+
+};
+console.log(bob.printFriends());
