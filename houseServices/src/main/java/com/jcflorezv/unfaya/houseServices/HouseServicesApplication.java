744c86fa467e6ceab3b8db827105ed4d18f68a6c
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-07-12 17:55:33 -0500

    house services base

---
+++ houseServices/src/main/java/com/jcflorezv/unfaya/houseServices/HouseServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jcflorezv.unfaya.houseServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseServicesApplication.class, args);
+	}
+
+}
---
+++ houseServices/src/test/java/com/jcflorezv/unfaya/houseServices/HouseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcflorezv.unfaya.houseServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
