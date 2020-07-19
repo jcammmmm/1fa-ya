bf8833fab2f327781974ad8a0ca8818808b9d6d2
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-07-18 22:26:51 -0500

    chaining promises

--- jsClientTests/src/index.js
+++ jsClientTests/src/index.js
@@ -87,8 +87,7 @@ class Theneable {
   }
 
   then(resolve, reject) {
-    alert(resolve);
-    console.log("test")
+    console.log("inside of Theneable.then()")
     setTimeout(() => resolve(this.num * 2), 1000);
   }
 }
@@ -108,12 +107,89 @@ let p = new Promise((resolve, reject) => {
   // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
   // In this example, we use setTimeout(...) to simulate async code. 
   // In reality, you will probably be using something like XHR or an HTML5 API.
-  setTimeout(function() {
-    resolve(new Theneable(333))
-    reject("fail.")
-  }, 2232)
+  setTimeout( function() {
+    resolve("Success on " + new Date())  // Yay! Everything went well!
+  }, 250) 
+}) 
+
+p.then((successMessage) => {
+  // successMessage is whatever we passed in the resolve(...) function above.
+  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+  console.log("Yay! " + successMessage) 
 });
 
-p.then((msj) => console.log(msj))
 
+/************************************************************
+ * MDN medium example
+ ************************************************************/
+// To experiment with error handling, "threshold" values cause errors randomly
+const THRESHOLD_A = 9; // can use zero 0 to guarantee error
+
+function tetheredGetNumber(resolve, reject) {
+  try {
+    setTimeout( 
+      function() {
+        const randomInt = Date.now();
+        const value = randomInt % 10;
+        try { 
+          if(value >= THRESHOLD_A) {
+            throw new Error(`Too large: ${value}`);
+          } 
+        } catch(msg) {
+            reject(`Error in callback ${msg}`); 
+        }
+      resolve(value);
+      return;
+    }, 3000);
+    // To experiment with error at set-up, uncomment the following 'throw'.
+    // throw new Error("Bad setup");
+  } catch(err) {
+    reject(`Error during setup: ${err}`);
+  }
+  return;
+}
+
+function determineParity(value) {
+  const isOdd = value % 2 ? true : false ;
+  const parityInfo = { theNumber: value, isOdd: isOdd };
+  return parityInfo;
+}
+
+function troubleWithGetNumber(reason) {
+  console.error(`Trouble getting number: ${reason}`);
+  throw -999; // must "throw" something, to maintain error state down the chain
+}
+
+function promiseGetWord(parityInfo) {
+  // The "tetheredGetWord()" function gets "parityInfo" as closure variable.
+  var tetheredGetWord = function(resolve,reject) {
+    const theNumber = parityInfo.theNumber;
+    const threshold_B = THRESHOLD_A - 1;
+    if(theNumber >= threshold_B) {
+      reject(`Still too large: ${theNumber}`);
+    } else {
+      parityInfo.wordEvenOdd = parityInfo.isOdd ? 'odd' : 'even';
+      resolve(parityInfo);
+    }
+    return;
+  }
+  return new Promise(tetheredGetWord);
+}
+
+(new Promise(tetheredGetNumber))
+  .then(determineParity,troubleWithGetNumber)
+  .then(promiseGetWord)
+  .then((info) => { 
+    console.log("Got: ",info.theNumber," , ", info.wordEvenOdd); 
+    return info; 
+  })
+  .catch((reason) => {
+    if(reason === -999) {
+      console.error("Had previously handled error");
+    }
+    else {
+      console.error(`Trouble with promiseGetWord(): ${reason}`);
+    }
+   })
+  .finally((info) => console.log("All done"));
 
